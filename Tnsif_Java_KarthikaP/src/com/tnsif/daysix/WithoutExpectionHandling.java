d9286e761edf3a38a5c2c974f3129c2967731ad2
admin <admin@nitish>
2025-10-09 11:10:22 +0530

    This is daysix java program.

---
+++ Tnsif_Java_KarthikaP/src/com/tnsif/daysix/WithoutExpectionHandling.java
@@ -0,0 +1,9 @@
+package com.tnsif.daysix;
+
+public class WithoutExpectionHandling {
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+
+}
